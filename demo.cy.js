ee61d3eea5c9d249f26d47a52eca4e6457ae95eb
NishadParekh <121786144+NishadParekh@users.noreply.github.com>
2023-02-07 11:47:30 +0530

    Add files via upload

---
+++ demo.cy.js
@@ -0,0 +1,9 @@
+describe('JustGitHub', function () {
+  it('h1', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://www.youtube.com/playlist?list=PLUDwpEzHYYLvA7QFkC1C0y0pDPqYS56iU');
+    cy.get('[aria-label="Home"] > #endpoint > #icon').click();
+    
+  })
+  
+  })
