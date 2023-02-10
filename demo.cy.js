dfde3c5cb27f19422b3dbe9065b8a4023c1c2a6e
NishadParekh <121786144+NishadParekh@users.noreply.github.com>
2023-02-10 12:05:10 +0530

    Delete demo.cy.js

--- demo.cy.js
+++
@@ -1,9 +0,0 @@
-describe('JustGitHub', function () {
-  it('h1', function() {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('https://www.youtube.com/playlist?list=PLUDwpEzHYYLvA7QFkC1C0y0pDPqYS56iU');
-    cy.get('[aria-label="Home"] > #endpoint > #icon').click();
-    
-  })
-  
-  })
